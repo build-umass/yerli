0eb36c33e67fd8338aaf611e206e9352a75c229f
Bryce Parkman <brycecparkman@gmail.com>
2021-05-05 12:09:47 -0400

    Added location marker

--- src/components/explore/BusinessCard.js
+++ src/components/explore/BusinessCard.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image';
+import { getDistance } from 'geolib';
 
-export default function BusinessCard({ businessTitle, backgroundPicture, data}) {
+export default function BusinessCard({ businessTitle, userLocation, backgroundPicture, data }) {
   return (
     <div>
-      <a href={'/explore?id=' + data.id} style={{margin: '0 0.35rem', display: 'block'}}>
+      <a href={'/explore?id=' + data.id} style={{ margin: '0 0.35rem', display: 'block' }}>
         <Card className="main-card" bg={'dark'} style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ), url("${backgroundPicture}")` }}>
           <Card.Body>
-            <Card.Title className="business-title">{businessTitle}</Card.Title>
+            <Card.Title className="business-title">{businessTitle}<br />
+              {userLocation !== null ?
+                <div className="mileage">
+                  <Image width={13} src={require("../../images/vector/Vector.png")} className='mileageImg' />
+                  {(getDistance({ latitude: userLocation.latitude, longitude: userLocation.longitude }, {
+                    latitude: data.lat,
+                    longitude: data.lon
+                  }) / 1609).toFixed(1) + ' mi'}
+                </div> : null}
+            </Card.Title>
           </Card.Body>
         </Card>
       </a>
-    </div>
+    </div >
   )
 }
