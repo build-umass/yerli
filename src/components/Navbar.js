7363a532319b7a4fd02eff7bd68cf4a38a0dfd93
Bryce Parkman <brycecparkman@gmail.com>
2021-05-31 19:07:08 -0400

    Updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,34 +5,32 @@ import '../App.css'
 
 export default function NavbarComponent() {
     return (
-        <Navbar style={{ backgroundColor: '#30C692' }} expand="sm">
-            <div id="navbar">
-                <Navbar.Brand href="/">
-                    <div className="Logo">
-                        <p>the Yerli.</p>
-                    </div>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link target='_blank' rel="noopener noreferrer" href='https://airtable.com/shrF1mYf40GTKswiv'>
-                            <div className="Nav-Text">
-                                <p>Feedback</p>
-                            </div>
-                        </Nav.Link>
-                        <Nav.Link target='_blank' rel="noopener noreferrer" href='https://share.hsforms.com/1yVWUTuaYTaScvCLCzXWF_g5c7qf'>
-                            <div className="Nav-Text">
-                                <p>Suggest a Business</p>
-                            </div>
-                        </Nav.Link>
-                        <Nav.Link target='_blank' rel="noopener noreferrer" href='https://www.theyerli.com/download'>
-                            <div className="Nav-Text">
-                                <p>Download the app</p>
-                            </div>
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </div>
+        <Navbar expand="md" id="navbar">
+            <Navbar.Brand href="https://www.theyerli.com/">
+                <div className="Logo">
+                    <p>the Yerli.</p>
+                </div>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link target='_blank' rel="noopener noreferrer" href='https://airtable.com/shrF1mYf40GTKswiv'>
+                        <div className="Nav-Text">
+                            Feedback
+                        </div>
+                    </Nav.Link>
+                    <Nav.Link target='_blank' rel="noopener noreferrer" href='https://share.hsforms.com/1yVWUTuaYTaScvCLCzXWF_g5c7qf'>
+                        <div className="Nav-Text">
+                            Suggest a Business
+                        </div>
+                    </Nav.Link>
+                    <Nav.Link target='_blank' rel="noopener noreferrer" href='https://www.theyerli.com/download'>
+                        <div className="Nav-Text">
+                            Download the app
+                        </div>
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar >
     )
 }
